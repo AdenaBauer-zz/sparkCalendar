4fa489a9a89e77b13f0662cfbcd95d9daed5a05f
AdenaBauer <adena32@gmail.com>
2015-08-12 09:39:03 -0700

    initial script file and css created, calendar embedded

--- script.js
+++ script.js
@@ -1,66 +1,66 @@
-      // Your Client ID can be retrieved from your project in the Google
-      // Developer Console, https://console.developers.google.com
-var CLIENT_ID = '231446828709-rern8mev478lpdd0pddn6ers73pnnulh.apps.googleusercontent.com
-';
+
+// Your Client ID can be retrieved from your project in the Google
+// Developer Console, https://console.developers.google.com
+var CLIENT_ID = '231446828709-rern8mev478lpdd0pddn6ers73pnnulh.apps.googleusercontent.com';
 
 var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 
 /**
-* Check if current user has authorized this application.
-*/
+ * Check if current user has authorized this application.
+ */
+ /*
 function checkAuth() {
   gapi.auth.authorize(
     {
-       'client_id': CLIENT_ID,
-        'scope': SCOPES,
-        'immediate': true
-     }, handleAuthResult);
+      'client_id': CLIENT_ID,
+      'scope': SCOPES,
+      'immediate': true
+    }, handleAuthResult);
 }
-
-/**
-* Handle response from authorization server.
-*
-* @param {Object} authResult Authorization result.
 */
+/**
+ * Handle response from authorization server.
+ *
+ * @param {Object} authResult Authorization result.
+ */
 function handleAuthResult(authResult) {
   var authorizeDiv = document.getElementById('authorize-div');
   if (authResult && !authResult.error) {
     // Hide auth UI, then load client library.
     authorizeDiv.style.display = 'none';
     loadCalendarApi();
-  } 
-  else {
+  } else {
     // Show auth UI, allowing the user to initiate authorization by
     // clicking authorize button.
-      authorizeDiv.style.display = 'inline';
+    authorizeDiv.style.display = 'inline';
   }
 }
 
-  /**
-  * Initiate auth flow in response to user clicking authorize button.
-  *
-  * @param {Event} event Button click event.
-  */
+/**
+ * Initiate auth flow in response to user clicking authorize button.
+ *
+ * @param {Event} event Button click event.
+ */
 function handleAuthClick(event) {
   gapi.auth.authorize(
-  {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
-  handleAuthResult);
+    {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+    handleAuthResult);
   return false;
 }
 
 /**
-* Load Google Calendar client library. List upcoming events
-* once client library is loaded.
-*/
+ * Load Google Calendar client library. List upcoming events
+ * once client library is loaded.
+ */
 function loadCalendarApi() {
-  gapi.client.load('calendar', 'v3'/*, listUpcomingEvents*/);
+  gapi.client.load('calendar', 'v3', listUpcomingEvents);
 }
 
 /**
-* Print the summary and start datetime/date of the next ten events in
-* the authorized user's calendar. If no events are found an
-* appropriate message is printed.
-*/
+ * Print the summary and start datetime/date of the next ten events in
+ * the authorized user's calendar. If no events are found an
+ * appropriate message is printed.
+ */
 function listUpcomingEvents() {
   var request = gapi.client.calendar.events.list({
     'calendarId': 'primary',
@@ -84,26 +84,19 @@ function listUpcomingEvents() {
         }
         appendPre(event.summary + ' (' + when + ')')
       }
-    } 
-    else {
+    } else {
       appendPre('No upcoming events found.');
     }
 
   });
 }
 
-function requestSpace(){
-  var request
-
-}
-
-
 /**
-* Append a pre element to the body containing the given message
-* as its text node.
-*
-* @param {string} message Text to be placed in pre element.
-*/
+ * Append a pre element to the body containing the given message
+ * as its text node.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
 function appendPre(message) {
   var pre = document.getElementById('output');
   var textContent = document.createTextNode(message + '\n');
